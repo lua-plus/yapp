58a8648482fcf06b2b5d82ae7aa88f2edeec0d5b
Zingle <oezingle@gmail.com>
2025-05-30 00:23:10 -0700

    add debug.env.getfenv/setfenv

---
+++ src/debug/env/getfenv.lua
@@ -0,0 +1,23 @@
+-- https://leafo.net/guides/setfenv-in-lua52-and-above.html
+
+---@diagnostic disable-next-line:deprecated
+return getfenv or (debug or {}).getfenv or (function()
+    local debug_getupvalue = (debug or {}).getupvalue
+    
+    assert(debug_getupvalue, "debug.getupvalue must exist")
+    
+    function getfenv(fn)
+        local i = 1
+        while true do
+            local name, val = debug_getupvalue(fn, i)
+            if name == "_ENV" then
+                return val
+            elseif not name then
+                break
+            end
+            i = i + 1
+        end
+    end
+
+    return getfenv
+end)()
---
+++ src/debug/env/setfenv.lua
@@ -0,0 +1,30 @@
+-- https://leafo.net/guides/setfenv-in-lua52-and-above.html
+
+---@diagnostic disable-next-line:deprecated
+return setfenv or (debug or {}).setfenv or (function ()
+    local debug_getupvalue = (debug or {}).getupvalue
+    local debug_upvaluejoin = (debug or {}).upvaluejoin
+
+    assert(debug_getupvalue and debug_upvaluejoin, "debug.getupvalue and debug.upvaluejoin must exist")
+
+    local function setfenv(fn, env)
+        local i = 1
+        while true do
+            local name = debug_getupvalue(fn, i)
+            if name == "_ENV" then
+                debug_upvaluejoin(fn, i, (function()
+                    return env
+                end), 1)
+                break
+            elseif not name then
+                break
+            end
+    
+            i = i + 1
+        end
+    
+        return fn
+    end
+    
+    return setfenv
+end)()
