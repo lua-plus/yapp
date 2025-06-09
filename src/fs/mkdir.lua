4962700244f47138ab32b74441703de5159006a9
Zingle <oezingle@gmail.com>
2025-06-09 12:12:57 -0700

    mkdir and mkdir -p

---
+++ src/fs/mkdir.lua
@@ -0,0 +1,30 @@
+local has_lfs, lfs = pcall(require, "lfs")
+
+local is_windows = require("src.os.is_windows")
+
+---@alias Yapp.Fs.Mkdir fun (path: string): nil
+
+if has_lfs then
+    ---@type Yapp.Fs.Mkdir
+    local function mkdir_lfs(path)
+        lfs.mkdir(path)
+    end
+    return mkdir_lfs
+end
+
+
+if is_windows then
+    ---@type Yapp.Fs.Mkdir
+    local function mkdir_windows(path)
+        os.execute(string.format("mkdir %q", path))
+    end
+    return mkdir_windows
+end
+
+
+---@type Yapp.Fs.Mkdir
+local function mkdir_unix(path)
+    os.execute(string.format("mkdir %q", path))
+end
+
+return mkdir_unix
---
+++ src/fs/mkdir_p.lua
@@ -0,0 +1,30 @@
+
+local dirname = require("src.fs.path.dirname")
+local split = require("src.string.split")
+local sep = require("src.fs.path.sep")
+local reduce = require("src.list.reduce")
+local join = require("src.fs.path.join")
+local exists = require("src.fs.exists")
+local mkdir = require("src.fs.mkdir")
+local is_dir = require("src.fs.is_dir")
+
+---@param path string
+local function mkdir_p (path)
+    local dir = dirname(path)
+    
+    local slices = split(dir, sep)
+    
+    reduce(slices, function (previous, slice)
+        local path = join(previous, slice)
+
+        if not exists(path) then
+            mkdir(path)
+        elseif not is_dir (path) then
+            error(string.format("Path %q is a file - directory expected!", path))
+        end
+
+        return path
+    end, dir:sub(1,1) == sep and sep or "")
+end
+
+return mkdir_p
