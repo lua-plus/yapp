4d1fc39b13336332e304aab5d3572a46815feda7
Zingle <oezingle@gmail.com>
2025-06-10 17:09:19 -0700

    spec for basename

---
+++ spec/fs/path/basename_spec.lua
@@ -0,0 +1,16 @@
+
+local basename = require("src.fs.path.basename")
+
+describe("basename", function ()
+    it("works well for a path", function ()
+        assert.equal("map.lua", basename("src/table/map.lua"))
+    end)
+
+    it("works for a trailing directory", function ()
+        assert.equal("table", basename("src/table/"))
+    end)
+
+    it("works for just a path", function ()
+        assert.equal("map.lua", basename("map.lua"))
+    end)
+end)
