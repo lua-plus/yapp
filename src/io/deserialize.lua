f9bff6d70ae57c4aa1bb8909a6758a82485d243f
Zingle <oezingle@gmail.com>
2025-06-10 17:09:34 -0700

    spelling mistakes

--- src/io/deserialize.lua
+++ src/io/deserialize.lua
@@ -1,9 +1,9 @@
 
---- Load a serialized value. BEWARE: This function performs aribtrary exection. 
+--- Load a serialized value. BEWARE: This function performs arbitrary execution. 
 ---@param str string
 ---@return any
 local function deserialize (str)
-    local get_chunk, err = load("return " .. str, "deserialize input")
+    local get_chunk, err = load("return " .. str, "deserialized input")
 
     if not get_chunk then
         error(err)
