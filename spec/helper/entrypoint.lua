1d49f956edf1fe2dc4818183f4b6a4fe1c3bbb38
Zingle <oezingle@gmail.com>
2025-07-15 18:04:18 -0700

    busted entrypoint for testing under Lua 5.1

---
+++ spec/helper/entrypoint.lua
@@ -0,0 +1,9 @@
+-- lua5.1/luajit doesn't support requiring my/module/init.lua as require("my.module")
+do
+    local sep = package.path:match("[/\\]")
+    local local_init = ("./?/init.lua"):gsub("/", sep)
+
+    if not package.path:match("%.[/\\]?[/\\]init%.lua") then
+        package.path = package.path .. ";" .. local_init
+    end
+end
